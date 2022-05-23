84c2168d9040d229a99a78b3627cffbf65927894
rmartikan <richard.martikan@avinty.com>
2022-05-23 18:37:18 +0200

    Add more unit tests.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,19 +11,20 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Account {
-	arg := CreateAccountParams{
+
+	args := CreateAccountParams{
 		Owner:    util.RandomUtils.RandomOwner(),
 		Balance:  util.RandomUtils.RandomMoney(),
 		Currency: util.RandomUtils.RandomCurrency(),
 	}
 
-	account, err := testQueries.CreateAccount(context.Background(), arg)
+	account, err := testQueries.CreateAccount(context.Background(), args)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
@@ -31,16 +32,17 @@ func createRandomAccount(t *testing.T) Account {
 }
 
 func TestListAccounts(t *testing.T) {
+
 	for i := 0; i < 10; i++ {
 		createRandomAccount(t)
 	}
 
-	arg := ListAccountsParams{
+	args := ListAccountsParams{
 		Limit:  5,
 		Offset: 5,
 	}
 
-	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	accounts, err := testQueries.ListAccounts(context.Background(), args)
 
 	require.NoError(t, err)
 	require.Len(t, accounts, 5)
@@ -50,6 +52,7 @@ func TestListAccounts(t *testing.T) {
 }
 
 func TestGetAccount(t *testing.T) {
+
 	acc1 := createRandomAccount(t)
 
 	acc2, err := testQueries.GetAccount(context.Background(), acc1.ID)
@@ -68,25 +71,27 @@ func TestCreateAccount(t *testing.T) {
 }
 
 func TestUpdateAccount(t *testing.T) {
+
 	acc1 := createRandomAccount(t)
 
-	arg := UpdateAccountParams{
+	args := UpdateAccountParams{
 		ID:      acc1.ID,
 		Balance: util.RandomUtils.RandomMoney(),
 	}
 
-	acc2, err := testQueries.UpdateAccount(context.Background(), arg)
+	acc2, err := testQueries.UpdateAccount(context.Background(), args)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, acc2)
 	require.Equal(t, acc1.ID, acc2.ID)
-	require.Equal(t, arg.Balance, acc2.Balance)
+	require.Equal(t, args.Balance, acc2.Balance)
 	require.Equal(t, acc1.Owner, acc2.Owner)
 	require.Equal(t, acc1.Currency, acc2.Currency)
 	require.WithinDuration(t, acc1.CreatedAt, acc2.CreatedAt, time.Second)
 }
 
 func TestDeleteAccount(t *testing.T) {
+
 	acc1 := createRandomAccount(t)
 
 	delErr := testQueries.DeleteAccount(context.Background(), acc1.ID)
