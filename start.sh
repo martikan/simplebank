46576b867866410adc54278b92f9da43275226ef
Richard Martikan <ric.martikan@gmail.com>
2022-07-15 23:53:30 +0200

    disable ssl

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration..."
-/app/migrate -path /app/migration -database "postgresql://$DATASOURCE_USER:$DATASOURCE_PASSWORD@$DATASOURCE_URL/$DATASOURCE_DB" -verbose up
+/app/migrate -path /app/migration -database "postgresql://$DATASOURCE_USER:$DATASOURCE_PASSWORD@$DATASOURCE_URL/$DATASOURCE_DB?sslmode=disable" -verbose up
 
 echo "start the app..."
 exec "$@"
