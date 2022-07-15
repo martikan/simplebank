de773bea3c37581229944f7fc0b1c9b956a4aac9
Richard Martikan <ric.martikan@gmail.com>
2022-07-15 22:45:30 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 set -e
 
 echo "run db migration..."
-echo "postgres://$DATASOURCE_USER:$DATASOURCE_PASSWORD@$SERVER_ADDRESS/$DATASOURCE_DB?sslmode=disable"
-/app/migrate -path /app/migration -database "postgres://$DATASOURCE_USER:$DATASOURCE_PASSWORD@$SERVER_ADDRESS/$DATASOURCE_DB?sslmode=disable" -verbose up
+/app/migrate -path /app/migration -database "postgresql://$DATASOURCE_USER:$DATASOURCE_PASSWORD@$SERVER_ADDRESS/$DATASOURCE_DB?sslmode=disable" -verbose up
 
 echo "start the app..."
 exec "$@"
